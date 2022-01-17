b14ff4a4057b2946141631403f7c7fcb79c00762
Kim Jioo <jwkim101201@gmail.com>
2022-01-17 09:18:51 +0900

    remove buttons

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -7,20 +7,7 @@ import { MessageActionRow, MessageButton } from 'discord.js'
 export default class PingCommand implements Command {
   /** 실행되는 부분입니다. */
   async run (interaction: I) {
-    const id = Math.random().toString(36).substring(2, 5)
-    const retryBtn = new MessageButton({ customId: id, label: '다시 측정', style: 'SUCCESS' })
-    const actionRow = new MessageActionRow({ components: [retryBtn] })
-
-    await interaction.editReply({ content: `Pong! ${client.ws.ping}ms`, components: [actionRow] })
-    const i = await interaction.channel?.awaitMessageComponent({
-      filter: (i) => i.customId === id && i.user.id === interaction.user.id,
-      componentType: 'BUTTON'
-    })
-
-    if (!i) return
-    await i.deferReply()
-
-    this.run(i as unknown as I)
+    await interaction.editReply({ content: `Pong! ${client.ws.ping}ms` })
   }
 
   /** 해당 명령어의 대한 설정입니다. */
