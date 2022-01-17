2cc65a1319873c4b5429db803970aae2f1103c15
Kim Jioo <jwkim101201@gmail.com>
2022-01-18 08:02:56 +0900

    remove comments, unnecessary imports

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,18 +1,14 @@
 import { client } from '..'
 import Command from '../interfaces/Command'
 import { I, D } from '../aliases/discord.js'
-import { MessageActionRow, MessageButton } from 'discord.js'
 
-/** 핑 명령어 */
 export default class PingCommand implements Command {
-  /** 실행되는 부분입니다. */
   async run (interaction: I) {
     await interaction.editReply({ content: `Pong! ${client.ws.ping}ms` })
   }
 
-  /** 해당 명령어의 대한 설정입니다. */
   metadata = <D>{
     name: 'ping',
-    description: 'Ping!'
+    description: 'Pong!'
   }
 }
