aa99dccbf486c74665701201aaf44b0456d7c6e2
Kim Jioo <jwkim101201@gmail.com>
2022-01-20 08:01:03 +0900

    fix: omg i didn't registered event

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import SlashHandler from './classes/SlashHandler'
 
 import onReady from './events/onReady'
 import onInteractionCreate from './events/onInteractionCreate'
+import onMessage from 'events/onMessage'
 
 // 봇 클라이언트를 생성합니다.
 export const client = new BotClient()
@@ -10,3 +11,4 @@ export const slash = new SlashHandler()
 
 client.onEvent('ready', onReady)
 client.onEvent('interactionCreate', onInteractionCreate)
+client.onEvent('message', onMessage)
