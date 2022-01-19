60efeb944ab8d5e7215db80942fa7aaf7772e02e
Kim Jioo <jwkim101201@gmail.com>
2022-01-20 08:01:42 +0900

    fix: cannot find module 'events/onMessage'

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import SlashHandler from './classes/SlashHandler'
 
 import onReady from './events/onReady'
 import onInteractionCreate from './events/onInteractionCreate'
-import onMessage from 'events/onMessage'
+import onMessage from './events/onMessage'
 
 // 봇 클라이언트를 생성합니다.
 export const client = new BotClient()
